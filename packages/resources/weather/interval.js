b26c7423c1231d34644c27a29e7b49c6d2ee5bb8
German Fernandez <germanfernandez0306@gmail.com>
2021-09-17 21:34:32 -0300

    ✅ add weather sync system

--- packages/resources/weather/interval.js
+++ packages/resources/weather/interval.js
@@ -1,9 +1,42 @@
-var weatherVariations = ['EXTRASUNNY', 'CLOUDS', 'FOGGY', 'RAIN', 'THUNDER', 'CLEARING', 'CLEAR'];
+const request = require("request");
 
-let actual = 0;
+const WEATHER_VALUES = {
+    Sunny: "EXTRASUNNY",
+    Clear: "EXTRASUNNY",
+    Cloudy: "CLOUDS",
+    Overcast: "OVERCAST",
+    Mist: "FOGGY",
+    Blizzard: "BLIZZARD",
+    Fog: "FOGGY",
+};
 
-setInterval(() => {
-    actual = weatherVariations.length != actual ? actual + 1 :  0;
-    mp.world.setWeatherTransition(weatherVariations[actual]);
-    console.log(weatherVariations[actual]);
-}, 3000);
+class Weather {
+    constructor(city) {
+        this.city = city;
+        this.get();
+        this.render();
+    }
+
+    get() {
+        request(
+            `http://api.weatherapi.com/v1/current.json?key=dc465e81fdd5444dade164858211709&q=${this.city}`,
+            (error, response, data) => {
+                if (dataFixed) {
+                    const dataFixed = JSON.parse(data);
+                    mp.world.setWeatherTransition(
+                        WEATHER_VALUES[dataFixed.current.condition.text] || RAIN,
+                        30000
+                    );
+                }
+            }
+        );
+    }
+
+    render() {
+        setInterval(() => {
+            this.get();
+        }, 60000);
+    }
+}
+
+const weather = new Weather("Los Angeles, CA");
