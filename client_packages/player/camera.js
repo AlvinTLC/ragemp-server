5e1f653fa0ad4368fa31655c51e707963971fa54
Germancito <germanfernandez0306@gmail.com>
2021-10-23 13:19:20 -0300

    refactor: use new operator

--- client_packages/player/camera.js
+++ client_packages/player/camera.js
@@ -1,13 +1,9 @@
 let viewMode = 0;
 
 mp.events.add("render", () => {
-    if (mp.game.controls.isControlJustReleased(0, 0)) {
-        if (viewMode === 0) {
-            viewMode = 4;
-        } else {
-            viewMode = 0;
-        }
-    }
-    mp.game.cam.setFollowPedCamViewMode(viewMode);
-    mp.game.cam.setFollowVehicleCamViewMode(viewMode);
+  if (mp.game.controls.isControlJustReleased(0, 0)) {
+    viewMode = viewMode === 0 ? 4 : 0;
+  }
+  mp.game.cam.setFollowPedCamViewMode(viewMode);
+  mp.game.cam.setFollowVehicleCamViewMode(viewMode);
 });
