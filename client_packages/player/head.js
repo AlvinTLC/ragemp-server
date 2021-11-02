cc5f1be5073f335748e9d6109be50799742d2cda
Germancito <germanfernandez0306@gmail.com>
2021-11-01 21:12:06 -0300

    refactor: add only in one file

---
+++ client_packages/player/head.js
@@ -0,0 +1,44 @@
+const playerLocal = mp.players.local;
+const playerFlags = {
+  occupied: "[...]",
+  ping: "///",
+};
+let playerText = "";
+
+let occupied = false;
+setInterval(() => {
+  if (mp.gui.cursor.visible && !occupied) {
+    occupied = true;
+    mp.events.callRemote("server:player:events:setVariable", "occupied", true);
+  } else if (!mp.gui.cursor.visible && occupied) {
+    occupied = false;
+    mp.events.callRemote("server:player:events:setVariable", "occupied", false);
+  }
+}, 1000);
+
+mp.events.add("render", () => {
+  mp.players.forEach((playerNearby) => {
+    let distance = 3.0;
+    if (distance < 5.0) {
+      let { x, y, z } = playerNearby.position;
+      const isOccupied = playerNearby.getVariable("occupied");
+      const hasHighPing = playerNearby.getVariable("highPing");
+
+      if (isOccupied && hasHighPing) {
+        playerText = `${playerFlags.occupied} ${playerFlags.ping}`;
+      } else if (isOccupied && !hasHighPing) {
+        playerText = `${playerFlags.occupied}`;
+      } else if (hasHighPing && !isOccupied) {
+        playerText = `${playerFlags.ping}`;
+      } else {
+        playerText = ``;
+      }
+
+      mp.game.graphics.drawText(playerText, [x, y, z + 0.905], {
+        font: 4,
+        color: [169, 169, 169, 255],
+        scale: [0.4, 0.4],
+      });
+    }
+  });
+});
